1041676a3ab6786879ac6b7458ef65738f233ecb
luanfv <luanfrancavieira@gmail.com>
2022-06-06 14:30:49 -0300

    feat: remove eslint & prettier

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+function Dashobard() {
+  return (
+    <Container>
+      <Title>Hello World!</Title>
+    </Container>
+  );
+}
+
+export { Dashobard };
---
+++ src/screens/Dashboard/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: purple;
+`;
+
+export { Container, Title };
