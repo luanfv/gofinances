05cf946760edfe9120e16003f9834ae17b0af656
luanfv <luanfrancavieira@gmail.com>
2022-06-06 16:36:58 -0300

    feat: react native responsive fontsize

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  User,
+  UserGreeting,
+  UserInfo,
+  UserName,
+  UserPhoto,
+  UserWrapper,
+} from './styles';
 
 function Dashobard() {
   return (
     <Container>
-      <Title>Hello World!</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <UserPhoto
+              source={{
+                uri: 'https://static.wikia.nocookie.net/naruto/images/3/33/Naruto_Uzumaki_%28Parte_I_-_HD%29.png/revision/latest?cb=20160316113315&path-prefix=pt-br'
+              }}
+            />
+
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+
+              <UserName>Naruto</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,16 +1,61 @@
 import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 
 const Container = styled.View`
   flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
+
+`;
+
+const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(42)}px;
+  background-color: ${({ theme }) => theme.colors.primary};
+
   justify-content: center;
   align-items: center;
-  background-color: ${({ theme }) => theme.colors.background};
+  flex-direction: row;
 `;
 
-const Title = styled.Text`
+const UserWrapper = styled.View`
+  width: 100%;
+  padding: 0 24px;
+`;
+
+const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const UserPhoto = styled.Image`
+  width: ${RFValue(48)}px;
+  height: ${RFValue(48)}px;
+  border-radius: 10px;
+`;
+
+const User = styled.View`
+  margin-left: 17px;
+`;
+
+const UserGreeting = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+`;
+
+const UserName = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
   font-family: ${({ theme }) => theme.fonts.bold};
-  font-size: 24px;
-  color: ${({ theme }) => theme.colors.primary};
+  font-size: ${RFValue(18)}px;
 `;
 
-export { Container, Title };
+export {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  UserPhoto,
+  User,
+  UserGreeting,
+  UserName,
+};
